c85608b12bb956ac9eb8d49d438f61f5ee9773af
Adrià Farrés <14farresa@gmail.com>
2018-05-25 15:47:46 +0200

    Add ability to overlap line and rectangle

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,13 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -15,10 +18,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        int width = 100, height = 400;
         HBox box = new HBox();
-        box.setAlignment(Pos.CENTER);
         for (int i = 0; i < 10; i++) {
-            Rectangle rectangle = new Rectangle(100, 400);
+            Rectangle rectangle = new Rectangle(width, height);
             if (i % 2 == 0)
                 rectangle.setFill(Color.BISQUE);
             else
@@ -26,8 +29,21 @@ public class Main extends Application {
             box.getChildren().add(rectangle);
         }
 
-        Scene scene = new Scene(box, 300, 250);
+        Pane pane = new Pane();
+        pane.getChildren().add(box);
 
+        int i = 4;
+        Node r = box.getChildren().get(i);
+        Bounds b = r.localToScene(r.getBoundsInLocal());
+
+        Line line = new Line();
+        line.setStartX(b.getMinX() + i * width);
+        line.setStartY(b.getMinY());
+        line.setEndX(b.getMaxX() + i * width);
+        line.setEndY(b.getMaxY());
+        pane.getChildren().add(line);
+
+        Scene scene = new Scene(pane, 300, 250);
         primaryStage.setTitle("Fiber optic simulation");
         primaryStage.setScene(scene);
         primaryStage.show();
