1f7ce1b8ba1f51885a3c5ac1f011e56f4aefa3ce
Adrià Farrés <14farresa@gmail.com>
2018-05-26 21:50:32 +0200

    Clean up `snell` and `calculate_light_time`

--- src/sample/Algorithm.java
+++ src/sample/Algorithm.java
@@ -6,104 +6,59 @@ public class Algorithm {
     public Algorithm() {
     }
 
-    Double[] snell(int n, Double[] space) { // parametros: numero de regiones del espacio y
-        // vector de indices de refraccion
-        Double[] sep_coord = new Double[n + 1]; // vector de coordenadas y(i)
-        Double[] new_sep_coord = new Double[n + 1];
-        boolean stop = false;
-        Double d = 0.05;
-        for (int i = 0; i <= n; ++i) { //rellena con randoms el vector de coordenadas
-            sep_coord[i] = ThreadLocalRandom.current().nextDouble(0.0, 10.0);
+    /**
+     * @param space Refraction index list
+     * @param d TODO
+     * @return List of updated y(i) coordinates
+     */
+    Double[] snell(Double[] space, double d) {
+        int num_regions = space.length;
+        Double[] y_coords = new Double[num_regions + 1];
+        for (int i = 0; i <= num_regions; i++) {
+            y_coords[i] = ThreadLocalRandom.current().nextDouble(0.0, 1.0);
         }
-        Double nuevotiempo, alt;
-        int reg;
-        Double tiempo0 = calculo_tiempo_luz(n, space, sep_coord); // tiempo que tarad la luz con
-        // las coordenadas actuales
+        Double time = calculate_light_time(space, y_coords);
         int cont = 0;
-        while (!stop) {
-            reg = ThreadLocalRandom.current().nextInt(1, n - 1);
-            alt = ThreadLocalRandom.current().nextDouble(-d, d);
-            ; //coge unas nuevas coordenadas para un punto de interseccion aleatorio.
-            new_sep_coord = sep_coord;
-            new_sep_coord[reg] = sep_coord[reg] + alt;
-            nuevotiempo = calculo_tiempo_luz(n, space, new_sep_coord);  //comprueba si con las
-            // nuevas coordenadas el tiempo es menor)
-            if (nuevotiempo < tiempo0) {
-                tiempo0 = nuevotiempo;
-                sep_coord = new_sep_coord;
+        while (true) {
+            int region = ThreadLocalRandom.current().nextInt(1, num_regions - 1);
+            Double alt = ThreadLocalRandom.current().nextDouble(-d, d);
+            Double[] new_y_coords = y_coords.clone();
+            new_y_coords[region] = y_coords[region] + alt;
+            Double new_time = calculate_light_time(space, new_y_coords);
+            if (new_time < time) {
+                time = new_time;
+                y_coords = new_y_coords;
                 cont = 0;
             } else {
-                ++cont;
+                cont++;
                 if (cont == 50) {
-                    stop = true;
+                    break;
                 }
             }
         }
-        return sep_coord;
+        return y_coords;
     }
 
-    Double calculo_tiempo_luz(int n, Double[] space, Double[] sep_coord) { //calculo del tiempo
-        // que tarda la luz en
-        Double velocidad_luz = 1.0;                                    // atravesar el espacio
-        // con esa direccion
-        Double tiempo_agg = 0.0;
-        Double distancia = 0.0;
-        Double velocidad_i = 0.0;
-        Tuple<Double, Double> fin = new Tuple<>(0.0, 0.0);
-        Tuple<Double, Double> ini = new Tuple<>(0.0, 0.0);
-        Double pp = 0.0;
-        Double tt = 0.0;
-        for (double i = 0.0; i < n; i = i + 1.0) {
-            fin.first = i + 1.0;
-            fin.second = sep_coord[(int) i + 1];
-            ini.first = i;
-            ini.second = sep_coord[(int) i];
-            pp = Math.pow(fin.first - ini.first, 2.0);
-            tt = Math.pow(fin.second - ini.second, 2.0);
-            distancia = Math.sqrt(pp + tt);
-            velocidad_i = velocidad_luz / space[(int) i];
-            tiempo_agg += distancia / velocidad_i;
+    /**
+     * @param space    Refraction index list
+     * @param y_coords y(i) coordinates list
+     * @return Time it takes for light to go through the given space
+     */
+    private Double calculate_light_time(Double[] space, Double[] y_coords) {
+        Double time = 0.0;
+        Tuple<Double, Double> end = new Tuple<>(0.0, 0.0);
+        Tuple<Double, Double> start = new Tuple<>(0.0, 0.0);
+        for (int i = 0; i < space.length; i++) {
+            end.first = i + 1.0;
+            end.second = y_coords[i + 1];
+            start.first = (double) i;
+            start.second = y_coords[i];
+            Double pp = Math.pow(end.first - start.first, 2.0);
+            Double tt = Math.pow(end.second - start.second, 2.0);
+            Double distance = Math.sqrt(pp + tt);
+            Double velocity = 1.0 / space[i];
+            time += distance / velocity;
         }
-        return tiempo_agg;
-    }
-
-
-    Double[] snellfijado(int n, Double[] space) { // parametros: numero de regiones del espacio y
-        // vector de indices de refraccion
-        Double[] sep_coord = new Double[n + 1]; // vector de coordenadas y(i)
-        Double[] new_sep_coord = new Double[n + 1];
-        boolean stop = false;
-        Double d = 0.5;
-        sep_coord[0] = 8.04;
-        sep_coord[2] = 1.51;
-        //for(int i = 1; i < n; ++i) { //rellena con randoms el vector de coordenadas
-        sep_coord[1] = ThreadLocalRandom.current().nextDouble(0.0, 10.0);
-        //}
-        Double nuevotiempo, alt;
-        int reg;
-        Double tiempo0 = calculo_tiempo_luz(n, space, sep_coord); // tiempo que tarad la luz con
-        // las coordenadas actuales
-        int cont = 0;
-        while (!stop) {
-            reg = 1;
-            alt = ThreadLocalRandom.current().nextDouble(-d, d);
-            ; //coge unas nuevas coordenadas para un punto de interseccion aleatorio.
-            new_sep_coord = sep_coord;
-            new_sep_coord[reg] = sep_coord[reg] + alt;
-            nuevotiempo = calculo_tiempo_luz(n, space, new_sep_coord);  //comprueba si con las
-            // nuevas coordenadas el tiempo es menor)
-            int o = 0;
-            if (nuevotiempo < tiempo0) {
-                tiempo0 = nuevotiempo;
-                sep_coord = new_sep_coord;
-                cont = 0;
-            } else {
-                ++cont;
-                if (cont == 500) {
-                    stop = true;
-                }
-            }
-        }
-        return sep_coord;
+        return time;
     }
 }
--- src/sample/Main_aux.java
+++ src/sample/Main_aux.java
@@ -3,9 +3,9 @@ package sample;
 public class Main_aux {
     public static void main(String[] args) {
         Algorithm a = new Algorithm();
-        Double[] espacio = {1.0, 1.55};
-        int n = 2;
-        Double[] res = a.snellfijado(n, espacio);
+        Double[] espacio = {1.0, 1.0, 1.0, 1.0, 1.0, 1.55, 1.55, 1.55, 1.55, 1.55};
+        int n = 10;
+        Double[] res = a.snell(espacio, 0.05);
         for (int i = 0; i < n + 1; ++i) {
             System.out.println(res[i]);
         }
