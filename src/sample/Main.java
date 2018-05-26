2a925133a528edbc80dcf9f029895b9fb48ae040
Adrià Farrés <14farresa@gmail.com>
2018-05-26 18:35:29 +0200

    Add prev/next buttons to cycle iterations

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -19,21 +20,21 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         int width = 100, height = 400;
-        HBox box = new HBox();
+        HBox rectangles_pane = new HBox();
         for (int i = 0; i < 10; i++) {
             Rectangle rectangle = new Rectangle(width, height);
             if (i % 2 == 0)
                 rectangle.setFill(Color.BISQUE);
             else
                 rectangle.setFill(Color.BURLYWOOD);
-            box.getChildren().add(rectangle);
+            rectangles_pane.getChildren().add(rectangle);
         }
 
-        Pane pane = new Pane();
-        pane.getChildren().add(box);
+        Pane main_pane = new Pane();
+        main_pane.getChildren().add(rectangles_pane);
 
         int i = 4;
-        Node r = box.getChildren().get(i);
+        Node r = rectangles_pane.getChildren().get(i);
         Bounds b = r.localToScene(r.getBoundsInLocal());
 
         Line line = new Line();
@@ -41,9 +42,16 @@ public class Main extends Application {
         line.setStartY(b.getMinY());
         line.setEndX(b.getMaxX() + i * width);
         line.setEndY(b.getMaxY());
-        pane.getChildren().add(line);
+        main_pane.getChildren().add(line);
 
-        Scene scene = new Scene(pane, 300, 250);
+        HBox buttons_pane = new HBox();
+        Button prev = new Button("<");
+        Button next = new Button(">");
+        buttons_pane.getChildren().add(prev);
+        buttons_pane.getChildren().add(next);
+        main_pane.getChildren().add(buttons_pane);
+
+        Scene scene = new Scene(main_pane, 300, 250);
         primaryStage.setTitle("Fiber optic simulation");
         primaryStage.setScene(scene);
         primaryStage.show();
