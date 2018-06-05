1f1c72abb9201bfe271d79b0c5bceeb2754cefbb
Adrià Farrés <14farresa@gmail.com>
2018-06-05 19:10:36 +0200

    ui: Clean up

--- src/sample/Main2.java
+++ src/sample/Main2.java
@@ -5,7 +5,6 @@ import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Slider;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -16,14 +15,15 @@ import javafx.stage.Stage;
 public class Main2 extends Application {
     private Pane main_pane;
     private VBox rectangles_pane;
-    private int width = 1000, height = 40, coordinate_index = 0;
+    private int width = 1000, height = 20, coordinate_index = 0;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     private void drawLines(int result_index, Double[] all_coordinates) {
-        Double[] current_coordinates = all_coordinates.get(result_index);
+//        Double[] current_coordinates = all_coordinates[result_index];
+        Double[] current_coordinates = new Double[10]; // FIXME: placeholder
         for (int i = 0; i < current_coordinates.length - 1; i++) {
             Node r = rectangles_pane.getChildren().get(i);
             Bounds b = r.localToScene(r.getBoundsInLocal());
@@ -41,12 +41,14 @@ public class Main2 extends Application {
     public void start(Stage primaryStage) {
         rectangles_pane = new VBox();
         rectangles_pane.setPadding(new Insets(80, 20, 20, 80));
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 35; i++) {
             Rectangle rectangle = new Rectangle(width, height);
-            rectangle.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+            if (i % 2 == 0)
+                rectangle.setFill(Color.BISQUE);
+            else
+                rectangle.setFill(Color.BURLYWOOD);
             rectangles_pane.getChildren().add(rectangle);
         }
-        // rectangles_pane.setAlignment(Pos.CENTER);
 
         // We force JavaFX to prerender the rectangles so that we know the exact position they
         // will occupy; otherwise, the first time we run drawLines() it will superpose all
@@ -57,25 +59,9 @@ public class Main2 extends Application {
         main_pane = new Pane();
         main_pane.getChildren().add(rectangles_pane);
 
-        Double[] res = Algorithm.snell(20, 37.5, 1.0, 1.5, 2, 1);
+//        Double[] res = Algorithm.snell(20, 37.5, 1.0, 1.5, 2, 1);
 
-        Slider slider = new Slider();
-        slider.setMin(0);
-        slider.setMax(res.size() - 1);
-        slider.setValue(0);
-        slider.setShowTickLabels(true);
-        slider.setShowTickMarks(true);
-        slider.setMajorTickUnit(1);
-        slider.setMinorTickCount(1);
-        slider.setBlockIncrement(1);
-        slider.setMinWidth(width);
-        slider.valueProperty().addListener((observableValue, oldValue, newValue) -> {
-            main_pane.getChildren().subList(4, main_pane.getChildren().size()).clear();
-            drawLines(newValue.intValue(), res);
-        });
-        main_pane.getChildren().add(slider);
-
-        drawLines(coordinate_index, res);
+//        drawLines(coordinate_index, res);
 
         Scene scene = new Scene(main_pane, 300, 250);
         primaryStage.setTitle("Fiber optic simulation");
@@ -83,7 +69,7 @@ public class Main2 extends Application {
         primaryStage.show();
 
         // The initial render is incorrect. Let's rerender it again.
-        main_pane.getChildren().subList(4, main_pane.getChildren().size()).clear();
-        drawLines(coordinate_index, res);
+        main_pane.getChildren().subList(1, main_pane.getChildren().size()).clear();
+//        drawLines(coordinate_index, res);
     }
 }
--- src/sample/Main_aux.java
+++ src/sample/Main_aux.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class Main_aux {
     public static void main(String[] args) {
-        Double[] espacio = {1.0, 1.0, 1.0, 1.0, 1.0, 1.55, 1.55, 1.55, 1.55, 1.55};
-        int n = 10;
-        ArrayList<Double[]> res = Algorithm.fermat(espacio, 0.05, 5);
+//        Double[] espacio = {1.0, 1.0, 1.0, 1.0, 1.0, 1.55, 1.55, 1.55, 1.55, 1.55};
+//        int n = 10;
+//        ArrayList<Double[]> res = Algorithm.fermat(espacio, 0.05, 5);
 //        for (int i = 0; i < n + 1; ++i) {
 //            System.out.println(res[i]);
 //        }
